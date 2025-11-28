d3aae4c2204fd139a61d7e3a24dfa94636e44a3a
glassbox71 <glassbox71@naver.com>
2025-11-28 22:14:34 +0900

    경고수정

--- src/components/SearchOverlay.jsx
+++ src/components/SearchOverlay.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import  { useRef, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './sass/SearchOverlay.scss';
 import { useProductStore } from '../store/useProductStore.js';
